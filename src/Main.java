4b81ebb0e664bdf447858ba373eee346c652bc5b
Mishator <voyteh89@mail.ru>
2023-03-11 17:28:46 +0300

    Выполнена Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+    public static void task1 () {
+        System.out.println("Задача 1");
+        int savingsFunds = 15000;
+        int sum = 0;
+        int month = 1;
+        while (sum < 2_459_000) {
+            sum += savingsFunds;
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + sum + " рублей");
+            month++;
+        }
+        System.out.println();
     }
 }
